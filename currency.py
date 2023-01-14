8efc28c35531a0e45c319797649ca02bdd931714
IL <igor.lerinc7@gmail.com>
2023-01-14 14:57:14 +0100

    Add files via upload

---
+++ currency.py
@@ -0,0 +1,25 @@
+
+import requests
+
+def currency_converter(amount, from_currency, to_currency):
+    # Use the 'requests' library to fetch the current exchange rate
+    url = f'https://api.exchangerate-api.com/v4/latest/{from_currency}'
+    res = requests.get(url)
+    data = res.json()
+    exchange_rate = data['rates'][to_currency]
+
+    # Multiply the amount by the exchange rate to get the converted amount
+    converted_amount = amount * exchange_rate
+
+    return converted_amount
+
+# Ask the user for the amount and currencies to convert
+amount = float(input("Enter the amount: "))
+from_currency = input("Enter the currency to convert from (e.g. USD, EUR): ")
+to_currency = input("Enter the currency to convert to (e.g. USD, EUR): ")
+
+# Use the currency_converter function to get the converted amount
+converted_amount = currency_converter(amount, from_currency, to_currency)
+
+# Print the result
+print(f"{amount} {from_currency} is equivalent to {converted_amount} {to_currency}")
